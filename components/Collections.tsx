021882c465116f9c43becdf29fe0eef42778ddf2
Phuc Mai <mariamaihong1309@gmail.com>
2024-03-05 16:37:37 -0500

    ui change

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -11,7 +11,7 @@ const Collections = async () => {
       {!collections || collections.length === 0 ? (
         <p className="text-body-bold">No collections found</p>
       ) : (
-        <div className="flex items-center justify-center gap-8">
+        <div className="flex flex-wrap items-center justify-center gap-8">
           {collections.map((collection: CollectionType) => (
             <Link href={`/collections/${collection._id}`} key={collection._id}>
               <Image
